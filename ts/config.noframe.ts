26b9716e38c4f222a09af203b84b8eb4f2255f0f
MPDieckmann <MPDieckmann@users.noreply.github.com>
2018-02-23 21:24:07 +0100

    Add files via upload

--- ts/config.noframe.ts
+++ ts/config.noframe.ts
@@ -1,5 +1,5 @@
 require.config({
-  baseUrl: "https://mpdieckmann.github.io/dev-test/",
+  baseUrl: "https://mpdieckmann.github.io/dev-test/js/",
   paths: {
     "ace": "https://ajaxorg.github.io/ace-builds/src-min-noconflict/ace"
   }
