fdcc17aa2336935b20432e2aad44ef2e5d384797
MPDieckmann <MPDieckmann@users.noreply.github.com>
2018-02-23 21:35:24 +0100

    Add files via upload

--- ts/setup.noframe.ts
+++ ts/setup.noframe.ts
@@ -1,10 +1,4 @@
-import "css!../css/setup";
 import { Console } from "./console";
-
-if (document.body.firstChild) {
-  document.body.insertBefore(Console.element, document.body.firstChild);
-} else {
-  document.body.appendChild(Console.element);
-}
+document.body.appendChild(Console.element);
 // @ts-ignore
 window.console = Console.getConsoleProxy(<Console.Global>window);
