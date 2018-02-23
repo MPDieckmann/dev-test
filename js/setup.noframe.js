2de97bdab068f5521c3a71622558e91a28f56273
MPDieckmann <MPDieckmann@users.noreply.github.com>
2018-02-23 21:35:05 +0100

    Add files via upload

--- js/setup.noframe.js
+++ js/setup.noframe.js
@@ -1,22 +1,7 @@
-(function (factory) {
-    if (typeof module === "object" && typeof module.exports === "object") {
-        var v = factory(require, exports);
-        if (v !== undefined) module.exports = v;
-    }
-    else if (typeof define === "function" && define.amd) {
-        define(["require", "exports", "css!../css/setup", "./console"], factory);
-    }
-})(function (require, exports) {
+define(["require", "exports", "./console"], function (require, exports, console_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    require("css!../css/setup");
-    const console_1 = require("./console");
-    if (document.body.firstChild) {
-        document.body.insertBefore(console_1.Console.element, document.body.firstChild);
-    }
-    else {
-        document.body.appendChild(console_1.Console.element);
-    }
+    document.body.appendChild(console_1.Console.element);
     window.console = console_1.Console.getConsoleProxy(window);
 });
 //# sourceMappingURL=setup.noframe.js.map
