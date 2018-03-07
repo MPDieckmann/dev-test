16cc3aa0cb29e1934a83e8afda644114e7b7aade
MPDieckmann <MPDieckmann@users.noreply.github.com>
2018-03-08 00:36:34 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,5 +6,7 @@ requirejs.config({
     paths: {
         "ace": "https://ajaxorg.github.io/ace-builds/src-noconflict/ace"
     }
-})(["ace","ace/mode-javascript","./setup"]);
+})(["require", "ace"], function (r) {
+  r(["ace/mode-javascript","./setup"]);
+});
 //# sourceMappingURL=config.js.map
